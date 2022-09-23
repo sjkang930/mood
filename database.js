271a0ad1266682f49855299f398d3ee1bfbf852d
sjkang930 <91238446+sjkang930@users.noreply.github.com>
2022-09-23 16:42:10 -0700

    changed port

--- database.js
+++ database.js
@@ -7,7 +7,8 @@ const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT,
 }).promise()
 
 export async function createMood(note, rate, date) {
