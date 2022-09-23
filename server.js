ae7a04a7ee7618fe8752299a2d87213fa7fa99fe
sjkang930 <91238446+sjkang930@users.noreply.github.com>
2022-09-23 16:48:41 -0700

    Changed path again

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ import express, { json } from "express";
 import { getMoods, createMood, updateMood, deleteMood } from "./database.js";
 const app = express();
 app.use(json());
+import * as path from "path";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 
 app.get("/api/moods", async (req, res) => {
     const moods = await getMoods();
@@ -22,7 +27,11 @@ app.delete("/api/moods/:id", async (req, res) => {
     const id = req.params.id;
     await deleteMood(id);
 })
-
+const disPath = path.join(__dirname, "./dist");
+app.use(express.static(disPath));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(disPath, "index.html"));
+})
 const port = process.env.PROT || 8080
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
